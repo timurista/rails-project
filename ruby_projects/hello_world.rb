e3ebe9489e367ccd6787d0b4f2f9a8c777fc6021
timurista <timothy.urista@gmail.com>
2018-10-12 10:50:50 -0400

    added basic hello world example with rb

---
+++ ruby_projects/hello_world.rb
@@ -0,0 +1,10 @@
+hello = "Hello World!"
+# puts hello
+# method multiplies 2 methods then returns it
+def say_hello(anything)
+  # write code
+  puts anything
+  puts "Hello World!"
+end
+
+say_hello "Welcome here"
